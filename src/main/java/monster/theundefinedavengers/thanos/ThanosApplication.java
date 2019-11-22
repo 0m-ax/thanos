8e823fd22a5545e26ec823ca41dd5bbd88a50674
Max Campbell <maxc@maxc.in>
2019-11-22 11:36:08 +0100

    inital commit

---
+++ src/main/java/monster/theundefinedavengers/thanos/ThanosApplication.java
@@ -0,0 +1,13 @@
+package monster.theundefinedavengers.thanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThanosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/monster/theundefinedavengers/thanos/ThanosApplicationTests.java
@@ -0,0 +1,13 @@
+package monster.theundefinedavengers.thanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
